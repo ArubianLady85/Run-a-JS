54193d0b87484f5aa91796db22dcb89cc74f3dd6
ArubianLady85 <88424294+ArubianLady85@users.noreply.github.com>
2021-11-26 21:15:05 +0100

    Add files via upload

---
+++ exercise 1/script.js
@@ -0,0 +1,18 @@
+console.log("Hello Winc Academy Students")
+
+/* This is a large sum*/
+function sum(a, b) {
+    return a * b;
+}
+
+console.log(sum(123041, 1823794));
+
+
+function sum(a, b) {
+    return a / b;
+}
+
+console.log(sum(637263, 54));
+
+//Answer 4 No I can't see my comment
+//Answer 6 yes I can still see the reuslt in the console 
